01bde5264eaf785e837fc4b8325bdea529f280a3
Gabriel Almeida <gab.galmeida@outlook.com>
2022-09-26 14:08:38 -0300

    Fixed bugs

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,21 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "_XQVmCQQk9FOL6bgtkZfJtifFucacL_4";
-
-// Replace this private key with your Goerli account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Beware: NEVER put real Ether into testing accounts
-const GOERLI_PRIVATE_KEY = "fec7ea0b3e439cb51126fa1d73a65c3477c76572af9105915f4c012081e88784";
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY]
-    }
-  }
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,29 +1,27 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-    /*
-        A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts, 
-        so whitelistContract here is a factory for instances of our Whitelist contract.
-    */
-   const whitelistContract = await ethers.getContractFactory("Whitelist");
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so whitelistContract here is a factory for instances of our Whitelist contract.
+  */
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-   //here we deploy the contract
-   const deployedWhitelistContract = await whitelistContract.deploy(10);
-   // 10 is the Maximum number of whitelisted addresses allowed
+  // here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // 10 is the Maximum number of whitelisted addresses allowed
 
-   // Wait for it to finish deploying
-   await deployedWhitelistContract.deployed();
+  // Wait for it to finish deploying
+  await deployedWhitelistContract.deployed();
 
-   // print the address of the deployed contract
-   console.log(
-    "Whitelist Contract Address: ", deployedWhitelistContract.address
-   );
+  // print the address of the deployed contract
+  console.log("Whitelist Contract Address:", deployedWhitelistContract.address);
 }
 
-//Call the main function and catch any errors
+// Call the main function and catch if there is any error
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
